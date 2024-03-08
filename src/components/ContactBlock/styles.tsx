120bb6530cb72eb415fd5559bd69d40c79949210
Teilor Souza Barcelos <tsb-@live.com>
2024-03-08 13:51:41 -0300

    feat: Contact page mobile style done

--- src/components/ContactBlock/styles.tsx
+++ src/components/ContactBlock/styles.tsx
@@ -12,4 +12,8 @@ export const ContactBlockFormInlineContainer = styled.div`
   display: flex;
   gap: ${rem(18)};
   width: 100%;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
